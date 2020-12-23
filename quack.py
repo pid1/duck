238163c3a4242793e486c2f7e410226979d2e46e
Jonathan Roemer <dev@roemersoftworks.com>
2020-12-23 01:48:02 +0000

    Add duplicate checking

--- quack.py
+++ quack.py
@@ -5,6 +5,7 @@ import argparse
 import configparser
 import os
 import random
+import re
 import string
 import sys
 
@@ -21,6 +22,8 @@ HTML_RESULT = """<!DOCTYPE html>
     <link rel="icon" type="image/png" href="data:image/png;base64,">
 </head>
 </html>""".format(target=args.url)
+
+DUP_CHECK = """<meta http-equiv="Refresh" content="0; url='{target}'" />""".format(target=args.url)
 
 # Attempt to load in the config file, gracefully handing the cases
 # where we can't for whatever reason.
@@ -47,7 +50,20 @@ if os.path.isfile(site_dir + '/' + SLUG + '.html'):
     print("Slug already exists, please re-run \
     to regenerate the slug. Quitting...")
     sys.exit(1)
-
+for root, dirs, files in os.walk(site_dir):
+    for name in files:
+        file_extension = os.path.splitext(name)
+        if file_extension[1] == ".html":
+            try:
+                to_check = open(site_dir + '/' + name, 'r')
+            except OSError as exception:
+                print(exception)
+                print("Failed to open site file for duplicate checking. Quitting...")
+                sys.exit(1)
+            if re.search(DUP_CHECK, to_check.read()):
+                print("This target already exists as slug " + file_extension[0])
+                sys.exit(0)
+            to_check.close()
 
 try:
     writefile = open(site_dir + '/' + SLUG + '.html', 'x')
