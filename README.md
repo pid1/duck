ee556bafc971b0d527b8b450df71da7f64011a72
Jonathan Roemer <dev@roemersoftworks.com>
2022-09-04 10:17:17 -0500

    Linting fixes

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # duck 🦆
+
 [![GitHub Super-Linter](https://github.com/pid1/duck/workflows/Linting/badge.svg)](https://github.com/marketplace/actions/super-linter)
 
 An HTML-based URL shortener for static sites.
@@ -6,7 +7,7 @@ An HTML-based URL shortener for static sites.
 
 ## Usage
 
-By updating `duck.ini` with the directory containing your Github Pages site (or any other static site), you can use this to quickly generate short URLs for web resources with no nonsense, no tracking, and no credit card required.
+By updating `duck.ini` with the directory containing your GitHub Pages site (or any other static site), you can use this to quickly generate short URLs for web resources with no nonsense, no tracking, and no credit card required.
 
 `./quack.py <url>` dumps out a small HTML file that contains a meta tag with a Refresh element and a target URL. This will redirect browsers to the provided target URL. The filename is the shorturl of the target resource. 
 
@@ -15,12 +16,13 @@ Optionally, pass the `--slug` argument to use a custom slug. This bypasses the c
 You can also configure the slug length by updating `duck.ini`.
 
 ## Demo
+
 You can see this in action at the following URLs.
 
-https://pid1.pw/duck redirects to https://github.com/pid1/duck
+`https://pid1.pw/duck` redirects to `https://github.com/pid1/duck`
 
-https://pid1.pw/GM redirects to https://youtube.com
+`https://pid1.pw/GM` redirects to `https://youtube.com`
 
-https://pid1.pw/fb redirects to https://facebook.com
+`https://pid1.pw/fb` redirects to `https://facebook.com`
 
-The Github Pages repository that powers the pid1.pw demo above is available [here](https://github.com/pid1/redirect).
+The GitHub Pages repository that powers the pid1.pw demo above is available [here](https://github.com/pid1/redirect).
--- quack.py
+++ quack.py
@@ -30,12 +30,14 @@ HTML_RESULT = f"""<!DOCTYPE html>
 def duplicate_url_check(checkstring, site_directory):
     """Given a site directory and a test string, check to see if we've already created
     a .html file containing this test string"""
-    for root, directory, files in os.walk(site_directory): #pylint: disable-unused-variable
+    for root, directory, files in os.walk(
+        site_directory
+        ): #pylint: disable-unused-variable
         for name in files:
             file_name = os.path.splitext(name)
             if file_name[1] == ".html":
                 try:
-                    to_check = open(site_directory  + '/' + name, 'r', encoding="utf-8")
+                    to_check = open(site_directory  + "/" + name, "r", encoding="utf-8")
                 except OSError as open_exception:
                     print(open_exception)
                     print("Failed to open site file for duplicate checking.")
@@ -44,18 +46,22 @@ def duplicate_url_check(checkstring, site_directory):
                     return 0
                 to_check.close()
 
+
 # Generate a random unused slug.
 # In the case of maximum recursion depth, we just bail out right now
 def sluggen(slug_length, site_directory):
     """Generate a random slug, checking for duplicates"""
     try:
-        slug = ''.join(random.choice(string.ascii_lowercase) for _ in range(slug_length))
-        while os.path.isfile(site_directory + '/' + slug + '.html'):
+        slug = ''.join(
+            random.choice(string.ascii_lowercase) for _ in range(slug_length)
+        )
+        while os.path.isfile(site_directory + "/" + slug + ".html"):
             sluggen(slug_length, site_directory)
         return slug
     except RecursionError as exception:
         print("Failed to generate a slug. Try increasing the maximum slug length.")
         sys.exit(1)
+
 
 def main():
     """Main function"""
@@ -63,13 +69,13 @@ def main():
     # where we can't for whatever reason.
     config = configparser.ConfigParser()
     try:
-        config.read('duck.ini')
+        config.read("duck.ini")
     except configparser.Error as exception:
         print(exception)
         print("Error parsing the config file.")
         sys.exit(1)
-    site_dir = config.get('config', 'sitedir')
-    slug_length = config.getint('options', 'sluglength')
+    site_dir = config.get("config", "sitedir")
+    slug_length = config.getint("options", "sluglength")
 
     # Ensure the site directory exists
     if not os.path.isdir(site_dir):
@@ -78,7 +84,7 @@ def main():
 
     # Ensure the slug doesn't exist already, if set
     if args.slug:
-        if os.path.isfile(site_dir + '/' + args.slug + '.html'):
+        if os.path.isfile(site_dir + "/" + args.slug + ".html"):
             if args.slug:
                 print("This slug is already in use.")
                 sys.exit(1)
@@ -101,7 +107,7 @@ def main():
 
     # Write out our Refresh file
     try:
-        writefile = open(site_dir + '/' + slug + '.html', 'x', encoding="utf-8")
+        writefile = open(site_dir + "/" + slug + ".html", "x", encoding="utf-8")
         writefile.write(HTML_RESULT)
         writefile.close()
     except OSError as exception:
@@ -111,6 +117,7 @@ def main():
 
     # Let the user know we succeeded and give them the slug
     print("Success! Slug is " + slug)
+
 
 if __name__ == "__main__":
     main()
